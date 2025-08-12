1fa1bcd602d73a2874c52f7019f57d4156304ac4
Lucas Eduardo <lucaseduardo168@gmail.com>
2025-08-12 14:07:30 -0300

    conexão com postgresql e criação de tabela de usuário

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,36 @@
+import os 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
 
+# carrega variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# inicializa extensões
+db = SQLAlchemy()
+migrate = Migrate()
+
+# cria a aplicação Flask
 def create_app():
+
     app = Flask(__name__)
 
+    # configurações da aplicação com a url do banco de dados
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # inicializa as extensões com a aplicação
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     with app.app_context():
+
+        # importa os modelos para que o SQLAlchemy possa reconhecê-los
+        from . import models
+
+        # importa e registra os blueprints
         from . import main
         app.register_blueprint(main.main)
+        
         return app
---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    access_level = db.Column(db.Integer, nullable=False, default=1)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
